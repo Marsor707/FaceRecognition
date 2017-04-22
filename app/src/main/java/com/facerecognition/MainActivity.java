04605b9933d7f307ebf884b3c82b050a60bc87b6
Marsor <369135912@qq.com>
2017-04-22 14:30:30 +0800

    添加了一个界面

--- app/src/main/java/com/facerecognition/MainActivity.java
+++ app/src/main/java/com/facerecognition/MainActivity.java
@@ -1,14 +1,12 @@
 package com.facerecognition;
 
 import android.content.Intent;
-import android.support.design.internal.BottomNavigationItemView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
--- app/src/main/java/com/facerecognition/ModeActivity.java
+++ app/src/main/java/com/facerecognition/ModeActivity.java
@@ -1,18 +1,33 @@
 package com.facerecognition;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Seven on 2017/4/22.
  */
 
 public class ModeActivity extends AppCompatActivity {
+    private Button extractMode;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mode);
+        init();
+    }
 
+    private void init() {
+        extractMode= (Button) findViewById(R.id.extractMode);
+        extractMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ModeActivity.this,ExtractActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
