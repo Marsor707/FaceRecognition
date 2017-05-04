3d722adeaea93d9087bfeec53af51d21f39b7456
marsor <369135912@qq.com>
2017-05-04 21:26:07 +0800

    1、利用face++实现了人像建模功能 https://www.faceplusplus.com.cn/

--- app/src/main/java/com/facerecognition/ExtractActivity.java
+++ app/src/main/java/com/facerecognition/ExtractActivity.java
@@ -1,18 +1,33 @@
 package com.facerecognition;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import utils.ImageUtil;
 
 /**
  * Created by Seven on 2017/4/22.
  */
 
 public class ExtractActivity extends AppCompatActivity {
+    private static final String TAG="ExtractActivity";
+    private Intent intent;
+    private ImageView faceModeImage;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.extract);
-
+        init();
+    }
+    private void init(){
+        intent=getIntent();
+        faceModeImage= (ImageView) findViewById(R.id.faceModeImage);
+        String Base64Data=intent.getStringExtra("base64");
+        Bitmap ModeImage= ImageUtil.base64ToBitmap(Base64Data);
+        faceModeImage.setImageBitmap(ModeImage);
     }
 }
--- app/src/main/java/com/facerecognition/ModeActivity.java
+++ app/src/main/java/com/facerecognition/ModeActivity.java
@@ -1,18 +1,41 @@
 package com.facerecognition;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.megvii.cloud.http.BodyOperate;
+import com.megvii.cloud.http.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+
+import utils.ImageUtil;
 
 /**
  * Created by Seven on 2017/4/22.
  */
 
 public class ModeActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE1=1;
+    private static final String TAG="ModeActivity";
+    private static final String key = "YbgrHdNtC0p9OPtPHVCKdZJMJZgg6v7k";//api_key
+    private static final String secret = "5d5CZ1Vp_k55cXNNpKQqdUwZFkl1vdfj";//api_secret
+    private Bitmap myBitMap;
+    private Bitmap modeBitMap;
     private Button extractMode;
+    private ImageButton addPhotoMode;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +45,71 @@ public class ModeActivity extends AppCompatActivity {
 
     private void init() {
         extractMode= (Button) findViewById(R.id.extractMode);
+        addPhotoMode= (ImageButton) findViewById(R.id.addPhotoMode);
+        addPhotoMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,REQUEST_CODE1);
+            }
+        });
         extractMode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(ModeActivity.this,ExtractActivity.class);
-                startActivity(intent);
+                if (myBitMap != null) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            BodyOperate bodyOperate = new BodyOperate(key, secret);
+                            try {
+                                Response response = bodyOperate.HumanBodySegment(null,null,ImageUtil.bitmapToBase64(myBitMap));
+                                JSONObject json=getJSONObject(response);
+                                final Intent intent=new Intent(ModeActivity.this,ExtractActivity.class);
+                                intent.putExtra("base64",json.getString("result"));
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        startActivity(intent);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                }
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==REQUEST_CODE1&&resultCode==RESULT_OK){
+            if(data!=null){
+                Uri uri = data.getData();
+                Log.i(TAG, "onActivityResult: "+uri.toString());
+                ContentResolver contentResolver = this.getContentResolver();
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri));
+                    myBitMap=bitmap;
+                    addPhotoMode.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                return;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private JSONObject getJSONObject(Response response) throws JSONException {
+        String res=new String(response.getContent());
+        JSONObject json=new JSONObject(res);
+        return json;
+    }
+
+
 }
